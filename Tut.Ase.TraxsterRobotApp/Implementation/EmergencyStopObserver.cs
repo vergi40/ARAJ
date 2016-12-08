d9ddea93913c82afdabb455a5357723ed3581ee1
jsnlnslt <jani.sulunsilta@student.tut.fi>
2016-12-08 21:08:52 +0200

    Basic functionality

--- Tut.Ase.TraxsterRobotApp/Implementation/EmergencyStopObserver.cs
+++ Tut.Ase.TraxsterRobotApp/Implementation/EmergencyStopObserver.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,13 +10,16 @@ namespace Tut.Ase.TraxsterRobotApp.Implementation
     class EmergencyStopObserver
     {
         public const int LOOP_WAIT_TIME = 100;
+        public const int SAFETY_DISTANCE = 20;
 
         private Robot _robot;
         private bool _stopped;
+        private MutualData _mutualData;
         public EmergencyStopObserver(Robot robot, MutualData mutualData)
         {
             _robot = robot;
             _stopped = true;
+            _mutualData = mutualData;
         }
 
         public async Task StartLogic()
@@ -27,24 +31,47 @@ namespace Tut.Ase.TraxsterRobotApp.Implementation
                 //
                 if (_stopped)
                 {
+                    Debug.WriteLine("Observer_stopped");
                     continue;
+
                 }
                 else
                 {
-                    // Run logic
 
-                    // Read filtered data 
-                    // TODO
+                    try
+                    {
+                        // Read filtered sensor values
+                        foreach (var sensor in _mutualData.ReadFilteredData())
+                        {
+                            Debug.WriteLine(sensor.Key);
+                            Debug.WriteLine(sensor.Value);
+                            // Check if sensor value (distance from wall) is too small
+                            if (sensor.Value < SAFETY_DISTANCE)
+                            {
+                                Debug.WriteLine("EMERGENCY STATE");
+                                // Throw exception if wall too close
+                                throw new EmergencyStopException();
+                            }
+                        }
+                    }
 
-                    // Analyze
-                    // TODO
-                    
+                    // Handle possible empty mutualData values
+                    catch (NullReferenceException)
+                    {
+                        Debug.WriteLine("Encountered NullReferenceException at observer");
+                    }
+                    // Prevent task silent crash
+                    catch (EmergencyStopException)
+                    {
+
+                    }
+
                 }
-
 
             }
 
         }
+
 
         public void Run()
         {
